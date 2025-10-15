012fe1052d17dbb827833fb9962b8141df08633d
Goutham N Kothari <gouthamnkothari24@gmail.com>
2025-10-15 17:32:29 +0530

    Create Diamond_Pattern.java

---
+++ Pattern Printing/Advance Pattern/Triangles/Diamond_Pattern.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Diamond_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of row : ");
+        int r = sc.nextInt();
+
+        for (int i = 1; i <= r; i++) {
+            // space
+            for (int j = 1; j <= (r - i); j++) {
+                System.out.print("  ");
+            }
+            // star
+            for (int j = 1; j <= ((2*i)-1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // reverse
+        for (int i = r; i >= 1; i--) {
+            // space
+            for (int j = 1; j <= (r - i); j++) {
+                System.out.print("  ");
+            }
+            // star
+            for (int j = 1; j <= ((2*i)-1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
